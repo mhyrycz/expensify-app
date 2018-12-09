51f460af913b3fab818479f6349aeaa562ca18fe
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2018-12-09 15:03:11 +0100

    old files deleted

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import IndecisionApp from './components/IndecisionApp.js';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
+const routes = (
+  <BrowserRouter>
+
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
