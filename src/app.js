0899c5629746379a5a5ed912ceb3acd78e6a6d94
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2018-12-09 16:10:49 +0100

    added react link component for client site routing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, NavLink, Route, Switch} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -30,15 +30,28 @@ const NotFoundPage = () => (
   </div>
 );
 
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}> Go Home </NavLink>
+    <NavLink to="/create" activeClassName="is-active"> Create Expense</NavLink>
+    <NavLink to="/edit" activeClassName="is-active"> Edit Expense </NavLink>
+    <NavLink to="/help" activeClassName="is-active"> Help </NavLink>
+  </header>
+);
+
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-      <Route path="/create" component={AddExpensePage}/>
-      <Route path="/edit" component={EditExpensePage}/>
-      <Route path="/help" component={HelpPage}/>
-      <Route component={NotFoundPage} />
-    </Switch>
+    <div>
+      <Header/>
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+        <Route path="/create" component={AddExpensePage}/>
+        <Route path="/edit" component={EditExpensePage}/>
+        <Route path="/help" component={HelpPage}/>
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 
