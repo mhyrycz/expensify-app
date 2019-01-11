26c6257bfec70fe6d5767895bc5e759bb305ca1a
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2019-01-11 11:46:24 +0100

    add and remove actions added for expences reducer. uuid library for id numbers added

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,10 +1,61 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
+
+//actions genererators
+const addExpense = (
+  { description = '',
+    note = '',
+    amount = 0,
+    createdAt = 0
+  } = {}
+) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+})
+
+const removeExpense = (
+  {
+    id,
+    description = '',
+    note = '',
+    amount = 0,
+    createdAt = 0
+  } = {}
+) => ({
+  type: 'REMOVE_EXPENSE',
+  expense: {
+    id,
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+
 
 // Expenses Reducer
 const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
+    case 'ADD_EXPENSE':
+      //return state.concat(action.expense)
+      //concat combine smth with smth and create new object, push change some objec (not true function)
+      //spread operator in reducer
+      return [
+        ...state,
+        action.expense
+      ];
+    case 'REMOVE_EXPENSE':
+      //return state.concat(action.expense)
+      //console.log (state.indexOf(action.expense))
+      return state.filter(({ id }) => id !== action.expense.id);
     default:
       return state;
   }
@@ -30,8 +81,17 @@ const store = createStore(
     filters: filtersReducer
   })
 );
+//console.log every time when there is a change
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
-console.log(store.getState());
+const addOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+const addSecond = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }));
+
+store.dispatch(removeExpense({ id: store.getState().expenses[0].id }));
+
+
 
 const demoState = {
   expenses: [{
