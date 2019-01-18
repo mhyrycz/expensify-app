2cc22d57bc767479663c603c17726661e9248fc1
Maciej Hyrycz <maciek.hyrycz@gmail.com>
2019-01-18 21:22:04 +0100

    11.103 input window and filter by text enabled, remove expense action implemented into react component

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseItem from './ExpenseListItem';
+import ExpenseListFilters from './ExpenseListFilters'
 import selectExpenses from '../selectors/expenses';
 
 const ExpenseList = (props) => (
   <div>
     <h1>Expenses List</h1>
+    <ExpenseListFilters />
     {props.expenses.map((expense) => (
       <ExpenseItem key={expense.id }{...expense}/>
     ))
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses'
 
 
-const ExpenseItem = ({ description, amount, createdAt }) => (
+
+const ExpenseItem = ({dispatch, id, description, amount, createdAt }) => (
   <div>
     <h3>{description}</h3>
     <p>{amount} - {createdAt}</p>
+    <button onClick={() => {
+      dispatch(removeExpense({ id })) // { id: id }
+    }}>Remove</button>
   </div>
 )
 
-export default ExpenseItem
+const mapStateToProps = (state) => {
+  return {
+    expenses: state.expenses
+  }
+}
+
+export default connect(mapStateToProps)(ExpenseItem)
