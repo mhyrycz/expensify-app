2ba068a8804d4c691fec3667b1cb62971eb7853f
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2019-01-18 13:19:33 +0100

    11.99 redux-expensify.js splited for small files and implemented to react app

---
+++ src/actions/filters.js
@@ -0,0 +1,22 @@
+export const setTextFilter = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text
+})
+
+export const sortByAmount = () => ({
+  type: 'SORT_BY_AMOUNT'
+})
+
+export const sortByDate = () => ({
+  type: 'SORT_BY_DATE'
+})
+
+export const setStartDate = (timestamp) => ({
+  type: 'SET_START_DATE',
+  timestamp
+})
+
+export const setEndDate = (timestamp) => ({
+  type: 'SET_END_DATE',
+  timestamp
+})
--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter'
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+import expensesSelector from './selectors/expenses'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+store.subscribe(() => {
+  const state = store.getState();
+  console.log(expensesSelector(state.expenses, state.filters));
+});
+
+
+const addOne = store.dispatch(addExpense({ description: 'Water bill', amount: 100, createdAt: 200 }));
+const addSecond = store.dispatch(addExpense({ description: 'Gas bill', amount: 400, createdAt: 300 }));
+const addTextFilter = store.dispatch(setTextFilter( 'water' ));
 
 
 
